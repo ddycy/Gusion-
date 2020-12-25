35c1c0eb23f69ced9c6983482e9fc126dd1727ca
zhuangzichen <zhuangzichen@cellpies.com>
2020-12-25 11:43:10 +0800

    test122518

--- app/Http/Controller/Api/ApiTest1.php
+++ app/Http/Controller/Api/ApiTest1.php
@@ -67,34 +67,8 @@ class ApiAuthController extends Controller{
         * @return test
        */
       class h(){}
-          /**
-        * h
-        *
-        * @return test
-       */
-      class h(){}
-          /**
-        * h
-        *
-        * @return test
-       */
-      class h(){}
-          /**
-        * h
-        *
-        * @return test
-       */
-      class h(){}          /**
-        * h
-        *
-        * @return test
-       */
-      class h(){}          /**
-        * h
-        *
-        * @return test
-       */
-      class h(){}
+
+
 
 
 
